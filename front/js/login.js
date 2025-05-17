90d3dac1dd7e13ca5a546da47b743e8e60957228
rime3-14 <rim.chihab-eddine@insa-lyon.fr>
2025-05-17 18:35:43 +0200

    added pages for: register, log-in and main page

---
+++ front/js/login.js
@@ -0,0 +1,6 @@
+document.querySelector('.login-form').addEventListener('submit', function (e) {
+  
+  e.preventDefault();// empeche l envoi reel du formulaire car pas encore lié au back 
+
+  alert("Login submitted (fonctionnalité à implémenter plus tard)");
+});
